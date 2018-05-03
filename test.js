0f51d61df14fcb48a57255c7872f527ff7a8063a
Stanley Louis <stanlouis@msn.com>
2018-05-03 09:31:30 -0400

    Add day to alert

---
+++ test.js
@@ -0,0 +1,20 @@
+let now  = new Date();
+now;
+
+let moonLanding = new Date("July 20, 1969 20:18");
+moonLanding;
+
+console.log(now - moonLanding);
+
+let daysOfTheWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+console.log(daysOfTheWeek[now.getDay()]);
+
