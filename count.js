2c29002c0631a6097923502fe21c6c69dc54df84
Stanley Louis <stanlouis@msn.com>
2018-05-04 14:12:48 -0400

    add count.js

---
+++ count.js
@@ -0,0 +1,31 @@
+const sonnet = `Let me not to the marriage of true minds
+Admit impediments. Love is not love
+Which alters when it alteration finds,
+Or bends with the remover to remove.
+O no, it is an ever-fixed mark
+That looks on tempests and is never shaken;
+It is the star to every wand'ring bark,
+Whose worth's unknown, although his height be taken.
+Love's not time's fool, though rosy lips and cheeks
+Within his bending sickle's compass come:
+Love alters not with his brief hours and weeks,
+But bears it out even to the edge of doom.
+  If this be error and upon me proved,
+  I never writ, nor no man ever loved.`;
+
+let uniques = new Map();
+
+let words = sonnet.match(/\S+/gm);
+
+
+for (let word of words){
+        if(!uniques.has(word)) {
+            uniques.set(word, 1);
+        } else {
+            let currentValue = uniques.get(word);
+            uniques.set(word, currentValue + 1)
+        }
+}
+
+console.log(uniques);
+console.log(uniques.size)
